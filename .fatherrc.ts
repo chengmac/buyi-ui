b1e38c4439c8c3fd7d840bdf122390d3033bb898
chengmac <chengmac@126.com>
2024-12-29 10:00:57 +0800

    feat: 添加打包时输出css配置

--- .fatherrc.ts
+++ .fatherrc.ts
@@ -2,5 +2,17 @@ import { defineConfig } from 'father';
 
 export default defineConfig({
   // more father config: https://github.com/umijs/father/blob/master/docs/config.md
-  esm: { output: 'dist' },
+  esm: {
+    output: 'dist',
+    transformer: 'babel',
+  },
+  extraBabelPlugins: [
+    [
+      './scripts/babel-less-to-css.js', // 把 js/ts 文件中的 '.less' 字符转为 '.css'
+      {
+        test: '\\.less',
+      },
+    ],
+  ],
+  plugins: ['./scripts/loader.ts'],
 });
---
+++ scripts/babel-less-to-css.js
@@ -0,0 +1,15 @@
+// babel-less-to-css.js
+module.exports = function () {
+  return {
+    visitor: {
+      ImportDeclaration(path) {
+        if (/\.less$/.test(path.node.source.value)) {
+          path.node.source.value = path.node.source.value.replace(
+            /\.less/,
+            '.css',
+          );
+        }
+      },
+    },
+  };
+};
